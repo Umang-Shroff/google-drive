1e0dd8699ca4f6a88771ca77c3139694ec186120
Umang <soyumang55@gmail.com>
2025-01-02 00:12:02 +0530

    user schema

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const userModel = require('../models/user.model')
 
 router.get("/test", (req, res) => {
   res.send("Test route");
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async(req, res) => {
     try {
       const { name, email, password } = req.body;
       console.log({ name, email, password });
-      res.status(200).json({ username: name, email: email, success: true });
+      
+      const newUser = await userModel.create({
+        username:name,
+        email,
+        password
+      })
+      res.status(200).json({newUser})
+
     } catch (error) {
       console.log("Error at register: ", error);
       res.status(500).json({ error: error });
