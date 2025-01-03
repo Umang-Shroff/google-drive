27ea7768e1ef1913da53853271f8362b62e31e35
Umang <soyumang55@gmail.com>
2025-01-03 22:29:16 +0530

    auth middleware set

--- backend/routes/index.routes.js
+++ backend/routes/index.routes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../config/multer.config")
+const fileModel = require("../models/files.model")
 
-
+router.post("/upload", upload.single("file"), async(req,res)=>{
+    
+})
 
 module.exports = router
